656d0f7e184eeff59ef6df8c1ccf310979986645
wireql <dmtr.off.p@gmail.com>
2025-04-07 21:31:55 +0300

    added find address with osm

---
+++ client/src/components/UI/OpenLayers/MapComponent.vue
@@ -0,0 +1,154 @@
+<script setup>
+import { Feature, Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import { OSM } from 'ol/source';
+import 'ol/ol.css';
+import { onMounted, ref } from 'vue';
+import { fromLonLat, toLonLat } from 'ol/proj';
+import InputGroup from '../InputGroup.vue';
+import axios from 'axios';
+import { Point } from 'ol/geom';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+
+const map = ref(null);
+const mapContainer = ref(null);
+const mapData = ref({
+    search: ''
+});
+const nominatimData = ref([]);
+const emit = defineEmits(['address-data', 'address-marker-coords']);
+
+const handleMap = (evt) => {
+    const coords = toLonLat(evt.coordinate);
+    const lon = coords[0].toFixed(6);
+    const lat = coords[1].toFixed(6);
+
+    emit('address-marker-coords', {
+        lon: lon,
+        lat: lat
+    });
+
+    const marker = new Feature({
+        geometry: new Point(fromLonLat([lon, lat])),
+        name: 'Моя метка'
+    });
+
+    marker.setStyle(
+        new Style({
+            image: new Icon({
+                src: 'https://openlayers.org/en/latest/examples/data/icon.png',
+                scale: 0.8
+            })
+        })
+    );
+
+    if (!window.vectorLayer) {
+        window.vectorLayer = new VectorLayer({
+            source: new VectorSource()
+        });
+        map.value.addLayer(window.vectorLayer);
+    }
+
+    window.vectorLayer.getSource().clear();
+    window.vectorLayer.getSource().addFeature(marker);
+
+    console.log(lat + ' ' + lon);
+};
+
+const action = async () => {
+    try {
+        const response = await axios.get(
+            'https://nominatim.openstreetmap.org/search?format=json&addressdetails=1&q=' +
+                mapData.value.search
+        );
+
+        nominatimData.value = response.data[0];
+
+        emit('address-data', nominatimData.value);
+        zoomToAddress([response.data[0].lon, response.data[0].lat]);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const zoomToAddress = (coordinates, zoomLevel = 18) => {
+    const projectedCoords = fromLonLat(coordinates);
+
+    map.value.getView().animate({
+        center: projectedCoords,
+        zoom: zoomLevel,
+        duration: 1000
+    });
+
+    const marker = new Feature({
+        geometry: new Point(projectedCoords),
+        name: 'Моя метка'
+    });
+
+    marker.setStyle(
+        new Style({
+            image: new Icon({
+                src: 'https://openlayers.org/en/latest/examples/data/icon.png',
+                scale: 0.8
+            })
+        })
+    );
+
+    if (!window.vectorLayer) {
+        window.vectorLayer = new VectorLayer({
+            source: new VectorSource()
+        });
+        map.value.addLayer(window.vectorLayer);
+    }
+
+    window.vectorLayer.getSource().clear();
+    window.vectorLayer.getSource().addFeature(marker);
+};
+
+const initMap = () => {
+    map.value = new Map({
+        target: mapContainer.value,
+        layers: [
+            new TileLayer({
+                source: new OSM()
+            })
+        ],
+        view: new View({
+            center: fromLonLat([39.7188, 47.222115]),
+            zoom: 12
+        })
+    });
+
+    map.value.on('click', handleMap);
+};
+
+onMounted(() => {
+    initMap();
+});
+</script>
+
+<template>
+    <div class="flex border border-gray-300">
+        <div class="max-w-[250px] w-full py-2 px-3">
+            <InputGroup
+                v-model="mapData.search"
+                label="Введите адрес"
+                placeholder="Ростов-на-Дону, ул. Малиновского ..."
+            />
+
+            <button
+                type="button"
+                v-on:click="action()"
+                class="text-sm bg-[#C1E0FF] text-white py-[6px] px-[9px] rounded-[6px] w-full hover:cursor-pointer mt-2"
+            >
+                <div class="flex items-center justify-center gap-[10px]">
+                    <div class="text-[#357CC5]">Найти</div>
+                </div>
+            </button>
+        </div>
+        <div ref="mapContainer" class="w-full h-[400px]"></div>
+    </div>
+</template>
--- client/src/views/Me/Address/AddressCreate.vue
+++ client/src/views/Me/Address/AddressCreate.vue
@@ -1,89 +1,114 @@
 <script setup>
-    import { notify } from '@kyvg/vue3-notification';
-    import { onMounted, ref } from 'vue'; 
-    import { useAuthStore } from '@/stores/auth';
-    import router from '@/router';
+import { notify } from '@kyvg/vue3-notification';
+import { onMounted, ref } from 'vue';
+import { useAuthStore } from '@/stores/auth';
+import router from '@/router';
 
-    import Delete from '@/components/Icons/delete.vue';
-    import Edit from '@/components/Icons/edit.vue';
-    import InputGroup from '@/components/UI/InputGroup.vue'
-    import { getAddressCategories, storeAddress } from '@/api/Address';
+import Delete from '@/components/Icons/delete.vue';
+import Edit from '@/components/Icons/edit.vue';
+import InputGroup from '@/components/UI/InputGroup.vue';
+import { getAddressCategories, storeAddress } from '@/api/Address';
+import MapComponent from '@/components/UI/OpenLayers/MapComponent.vue';
 
-    const authStore = useAuthStore()
+const authStore = useAuthStore();
 
-    const actionLoading = ref(false);
-    const loading = ref(false);
-    const addressCategories = ref(null);
-    const fields = [
-        'country', 'region', 'city', 'street', 'building', 'flat', 'latitude', 'longitude', 'address_category_id'
-    ];
+const actionLoading = ref(false);
+const loading = ref(false);
+const addressCategories = ref(null);
+const fields = [
+    'country',
+    'region',
+    'city',
+    'street',
+    'building',
+    'flat',
+    'latitude',
+    'longitude',
+    'address_category_id'
+];
 
-    const data = ref(Object.fromEntries(fields.map(field => [field, null])));
-    const data__errors = ref(Object.fromEntries(fields.map(field => [field, []])));
+const data = ref(Object.fromEntries(fields.map((field) => [field, null])));
+const data__errors = ref(
+    Object.fromEntries(fields.map((field) => [field, []]))
+);
 
-    const resetErrors = () => {
-        Object.keys(data__errors.value).forEach(key => {
-            data__errors.value[key] = [];
+const resetErrors = () => {
+    Object.keys(data__errors.value).forEach((key) => {
+        data__errors.value[key] = [];
+    });
+};
+
+const action = async () => {
+    actionLoading.value = true;
+
+    try {
+        resetErrors();
+
+        const response = await storeAddress(data.value, authStore.token);
+
+        notify({
+            title: 'Добавление',
+            text: response.data.message,
+            type: 'success'
         });
-    };
+        actionLoading.value = false;
 
-    const action = async () => {
-        actionLoading.value = true;
-
-        try {
-            resetErrors()   
-            
-            const response = await storeAddress(data.value, authStore.token);
-
+        router.push('/me/address');
+    } catch (err) {
+        if (err.status >= 500) {
             notify({
-                title: "Добавление",
-                text: response.data.message,
-                type: 'success'
-            });
-            actionLoading.value = false;
-
-            router.push('/me/address');
-        } catch (err) {
-            if(err.status >= 500) {
-                notify({
-                    title: "API",
-                    text: "Ошибка сервера. Попробуйте позже.",
-                    type: 'error'
-                });
-
-                return;
-            }
-            
-            const errors = err.response?.data?.errors || {};
-            Object.keys(errors).forEach(key => {
-                if (data__errors.value[key] !== undefined) {
-                    data__errors.value[key] = errors[key];
-                }
-            });
-        } finally {
-            actionLoading.value = false;
-        }
-    }
-
-    onMounted(async () => {
-        loading.value = true;
-
-        try {
-            const categoriesRes = await getAddressCategories(authStore.token);
-
-            addressCategories.value = categoriesRes.data.data;
-        } catch (err) {
-            notify({
-                title: "Ошибка",
-                text: "Не удалось загрузить данные. Попробуйте позже.",
+                title: 'API',
+                text: 'Ошибка сервера. Попробуйте позже.',
                 type: 'error'
             });
-        } finally {
-            loading.value = false;
+
+            return;
         }
 
-    })   
+        const errors = err.response?.data?.errors || {};
+        Object.keys(errors).forEach((key) => {
+            if (data__errors.value[key] !== undefined) {
+                data__errors.value[key] = errors[key];
+            }
+        });
+    } finally {
+        actionLoading.value = false;
+    }
+};
 
+const handleAddressData = (dataRes) => {
+    data.value.country = dataRes.address.country;
+    data.value.region = dataRes.address.state;
+    data.value.city = dataRes.address.city;
+    data.value.street = dataRes.address.road;
+    data.value.building = dataRes.address.house_number;
+
+    data.value.latitude = dataRes.lat;
+    data.value.longitude = dataRes.lon;
+};
+
+const handleAddressCoords = (dataRes) => {
+    data.value.latitude = dataRes.lat;
+    data.value.longitude = dataRes.lon;
+};
+
+onMounted(async () => {
+    loading.value = true;
+
+    try {
+        const categoriesRes = await getAddressCategories(authStore.token);
+
+        addressCategories.value = categoriesRes.data.data;
+    } catch (err) {
+        notify({
+            title: 'Ошибка',
+            text: 'Не удалось загрузить данные. Попробуйте позже.',
+            type: 'error'
+        });
+    } finally {
+        loading.value = false;
+    }
+});
 </script>
 
 <template>
@@ -93,22 +118,36 @@
             <div class="text-xs">Добавление нового адреса</div>
         </div>
         <div class="flex items-center gap-[10px] mt-2">
-            <router-link :to="{'name': 'AddressIndex'}" class="text-sm border border-dark-50 py-[5px] px-[9px] rounded-[6px] w-auto hover:cursor-pointer">
+            <router-link
+                :to="{ name: 'AddressIndex' }"
+                class="text-sm border border-dark-50 py-[5px] px-[9px] rounded-[6px] w-auto hover:cursor-pointer"
+            >
                 <div class="flex items-center gap-[10px]">
-                    <Delete color="black"/>
+                    <Delete color="black" />
                     <div class="">Отмена</div>
                 </div>
             </router-link>
-            <button type="button" v-on:click="action()" class="text-sm bg-[#C1E0FF] text-white py-[6px] px-[9px] rounded-[6px] w-auto hover:cursor-pointer">
+            <button
+                type="button"
+                v-on:click="action()"
+                class="text-sm bg-[#C1E0FF] text-white py-[6px] px-[9px] rounded-[6px] w-auto hover:cursor-pointer"
+            >
                 <div class="flex items-center gap-[10px]">
-                    <Edit color="#357CC5"/>
+                    <Edit color="#357CC5" />
                     <div class="text-[#357CC5]">Сохранить</div>
                 </div>
             </button>
         </div>
     </div>
 
-    <hr class="border-gray-300 my-[24px]">
+    <hr class="border-gray-300 my-[24px]" />
+
+    <MapComponent
+        @address-data="handleAddressData"
+        @address-marker-coords="handleAddressCoords"
+    />
+
+    <hr class="border-gray-300 my-[24px]" />
 
     <div class="mt-6 pb-6">
         <div class="grid grid-cols-12 gap-5">
@@ -117,37 +156,85 @@
             </div>
             <div class="col-span-12 sm:col-span-8 xl:col-span-4">
                 <div class="flex flex-col gap-6">
-                    <InputGroup v-model="data.country" :error="data__errors.country[0]" label="Страна" placeholder="Россия"/>
-                    <InputGroup v-model="data.region" :error="data__errors.region[0]" label="Регион" placeholder="Ростовская область"/>
-                    <InputGroup v-model="data.city" :error="data__errors.city[0]" label="Город, населенный пункт" placeholder="Ростов-на-Дону"/>
-                    <InputGroup v-model="data.street" :error="data__errors.street[0]" label="Улица" placeholder="Малиновского"/>
-                    <InputGroup v-model="data.building" :error="data__errors.building[0]" label="Дом, строение" placeholder="125"/>
-                    <InputGroup v-model="data.flat" :error="data__errors.flat[0]" label="Квартира" placeholder="125"/>
+                    <InputGroup
+                        v-model="data.country"
+                        :error="data__errors.country[0]"
+                        label="Страна"
+                        placeholder="Россия"
+                    />
+                    <InputGroup
+                        v-model="data.region"
+                        :error="data__errors.region[0]"
+                        label="Регион"
+                        placeholder="Ростовская область"
+                    />
+                    <InputGroup
+                        v-model="data.city"
+                        :error="data__errors.city[0]"
+                        label="Город, населенный пункт"
+                        placeholder="Ростов-на-Дону"
+                    />
+                    <InputGroup
+                        v-model="data.street"
+                        :error="data__errors.street[0]"
+                        label="Улица"
+                        placeholder="Малиновского"
+                    />
+                    <InputGroup
+                        v-model="data.building"
+                        :error="data__errors.building[0]"
+                        label="Дом, строение"
+                        placeholder="125"
+                    />
+                    <InputGroup
+                        v-model="data.flat"
+                        :error="data__errors.flat[0]"
+                        label="Квартира"
+                        placeholder="125"
+                    />
                     <div class="flex flex-col gap-[5px] w-full">
                         <label class="text-sm opacity-[60%]">Категория</label>
-                        <select v-model="data.address_category_id" class="text-sm w-full border border-gray-300 focus:border-gray-700 bg-dark-100 py-[6px] px-[9px] rounded-[6px]">
+                        <select
+                            v-model="data.address_category_id"
+                            class="text-sm w-full border border-gray-300 focus:border-gray-700 bg-dark-100 py-[6px] px-[9px] rounded-[6px]"
+                        >
                             <option
                                 v-for="category in addressCategories"
                                 :key="category.id"
                                 :value="category.id"
                             >
-                            {{ category.name }}
+                                {{ category.name }}
                             </option>
                         </select>
-                        <p v-if="data__errors.address_category_id !== null" class="text-red-300 text-xs">{{ data__errors.address_category_id[0] }}</p>
+                        <p
+                            v-if="data__errors.address_category_id !== null"
+                            class="text-red-300 text-xs"
+                        >
+                            {{ data__errors.address_category_id[0] }}
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
-        <hr class="border-gray-300 my-[24px]">
+        <hr class="border-gray-300 my-[24px]" />
         <div class="grid grid-cols-12 gap-5">
             <div class="col-span-12 sm:col-span-4 xl:col-span-3">
                 <div class="text-sm font-bold">Координаты</div>
             </div>
             <div class="col-span-12 sm:col-span-8 xl:col-span-4">
                 <div class="flex flex-col gap-6">
-                    <InputGroup v-model="data.latitude" :error="data__errors.latitude[0]" label="Широта" placeholder="47.238704"/>
-                    <InputGroup v-model="data.longitude" :error="data__errors.longitude[0]" label="Долгота" placeholder="39.614897"/>
+                    <InputGroup
+                        v-model="data.latitude"
+                        :error="data__errors.latitude[0]"
+                        label="Широта"
+                        placeholder="47.238704"
+                    />
+                    <InputGroup
+                        v-model="data.longitude"
+                        :error="data__errors.longitude[0]"
+                        label="Долгота"
+                        placeholder="39.614897"
+                    />
                 </div>
             </div>
         </div>
